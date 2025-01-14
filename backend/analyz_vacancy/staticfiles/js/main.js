11594db96b348c178ec0163005fa50085c9e3045
wrwsc <destr0yka23@gmail.com>
2025-01-15 03:23:12 +0500

    add tables

--- backend/analyz_vacancy/staticfiles/js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const burger = document.getElementById("burger");
-    const sideMenu = document.getElementById("sideMenu");
-    burger.addEventListener("click", () => {
-        sideMenu.classList.toggle("active");
-    });
-});
