6532bca3f051b57307ca30456889d121c75f7b85
wrwsc <destr0yka23@gmail.com>
2025-01-21 18:04:21 +0500

    update p

--- backend/analyz_vacancy/stat/stat_vacancy.py
+++ backend/analyz_vacancy/stat/stat_vacancy.py
@@ -5,6 +5,8 @@ import matplotlib
 matplotlib.use('Agg')
 import requests
 from xml.etree import ElementTree as ET
+
+
 def get_exchange_rates():
     url = "https://www.cbr-xml-daily.ru/daily_utf8.xml"
     response = requests.get(url)
@@ -19,12 +21,15 @@ def get_exchange_rates():
             nominal = int(currency.find("Nominal").text)
             exchange_rates[char_code] = {"rate": rate / nominal}
         return exchange_rates
+
+
 file_path = '../statistics/vacancies_2024.csv'
 data = pd.read_csv(file_path, low_memory=False)
 data['published_at'] = pd.to_datetime(data['published_at'], errors='coerce', utc=True)
 data = data.dropna(subset=['published_at'])
 data['year'] = data['published_at'].dt.year
 exchange_rates = get_exchange_rates()
+
 def convert_to_rub(row, exchange_rates):
     if pd.notnull(row['salary_from']) or pd.notnull(row['salary_to']):
         avg_salary = np.nanmean([row['salary_from'], row['salary_to']])
